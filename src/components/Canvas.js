688bace98ad45b2c7adabbb60b48b962da1863b5
DevanshSampat <devansh.sampat@gmail.com>
2025-05-06 22:42:56 +0530

    canvas removal supported

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,11 +1,55 @@
 import * as fabric from "fabric"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import "../styles/Canvas.css"
 
-function Canvas({ config }) {
+function Canvas({ config, searchForRef, setCanvasRef, showPreviousButton, showNextButton, setCanvasIndex, index }) {
     const canvasref = useRef(null);
     const fabricCanvasRef = useRef(null);
+    const [imageWidth, setImageWidth] = useState(config.imageWidthScale * window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [canvasWidth, setCanvasWidth] = useState(config.canvasWidthScale * window.innerWidth);
+    const [canvasHeight, setCanvasHeight] = useState(canvasWidth * config.canvasAspectRatio);
+    const [canvasMarginLeft, setCanvasMarginLeft] = useState(config.canvasMarginLeftScale * windowWidth);
+    const [canvasMarginTop, setCanvasMarginTop] = useState(config.canvasMarginTopScale * windowWidth);
+    const [canvasMarginRight, setCanvasMarginRight] = useState(config.canvasMarginRightScale * windowWidth);
+    const [canvasMarginBottom, setCanvasMarginBottom] = useState(config.canvasMarginBottomScale * windowWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setImageWidth(config.imageWidthScale * window.innerWidth)
+            setWindowWidth(window.innerWidth);
+            setCanvasWidth(config.canvasWidthScale * window.innerWidth);
+            setCanvasHeight(config.canvasWidthScale * window.innerWidth * config.canvasAspectRatio);
+            setCanvasMarginLeft(config.canvasMarginLeftScale * window.innerWidth);
+            setCanvasMarginTop(config.canvasMarginTopScale * window.innerWidth);
+            setCanvasMarginRight(config.canvasMarginRightScale * window.innerWidth);
+            setCanvasMarginBottom(config.canvasMarginBottomScale * window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+        if (!canvasref.current) {
+            const canvas = new fabric.Canvas(fabricCanvasRef.current);
+            canvasref.current = canvas;
+        }
+        const canvas = canvasref.current;
+        if (localStorage.getItem(config.id)) {
+            canvas.loadFromJSON(JSON.parse(localStorage.getItem(config.id)), () => {
+                canvas.renderAll();
+            });
+        }
+        return () => window.removeEventListener("resize", handleResize);
+        //eslint-disable-next-line
+    }, [])
     const [showPreview, setShowPreview] = useState(false);
     const [imageData, setImageData] = useState();
+
+    const saveCanvas = () => {
+        if (!canvasref.current) {
+            const canvas = new fabric.Canvas(fabricCanvasRef.current);
+            canvasref.current = canvas;
+        }
+        const canvas = canvasref.current;
+        localStorage.setItem(config.id, JSON.stringify(canvas.toJSON()));
+    }
+
     const handleAddImage = (e) => {
         if (!canvasref.current) {
             const canvas = new fabric.Canvas(fabricCanvasRef.current);
@@ -30,48 +74,80 @@ function Canvas({ config }) {
                 canvas.add(image)
                 canvas.centerObject(image)
                 canvas.setActiveObject(image)
+                canvas.renderAll();
             }
         }
     }
     return (
-        <div className="container">
+        <div className="canvas-container">
+            <div className="canvas-background-image-area">
+                <img
+                    src={config.image} width={imageWidth} alt="background"
+                ></img>
+                {showPreview &&
+                    <div style={{ width: "70vw", height: "100vh", position: "absolute", zIndex: 50, alignItems: "center", justifyContent: "center", display: "flex" }}>
+                        <div style={{ ...config.canvasHolderStyle }} />
+                    </div>}
 
-            <img
-                src={config.image}
-            ></img>
+                {<div style={{ width: "70vw", height: "100vh", position: "absolute", zIndex: 50, alignItems: "center", justifyContent: "center", display: "flex" }}>
+                    <div style={{ border: `${showPreview ? "1px solid transparent" : config.canvasHolderStyle.border}`, width: canvasWidth, height: canvasHeight, marginLeft: canvasMarginLeft, marginTop: canvasMarginTop, marginRight: canvasMarginRight, marginBottom: canvasMarginBottom }}>
+                        <canvas
+                            width={canvasWidth}
+                            height={canvasHeight}
+                            ref={fabricCanvasRef}
+                            style={{ width: canvasWidth, height: canvasHeight }}
+                        />
+                    </div>
+                </div>}
+            </div>
 
-            {!showPreview && <input
-                type="file"
-                accept="image/*"
-                label="Add Image"
-                style={config.inputStyle}
-                onChange={handleAddImage}
-            />}
 
-            {showPreview && <div style={{ ...config.canvasHolderStyle, zIndex: 50, border: "none" }}></div>}
+            <div className="control-area">
 
-            {<div style={!showPreview ? config.canvasHolderStyle : { ...config.canvasHolderStyle, border: "1px solid transparent" }}>
-                <canvas
-                    width={config.canvasHolderStyle.width}
-                    height={config.canvasHolderStyle.height}
-                    ref={fabricCanvasRef}
-                    style={config.canvasStyle}
-                />
-            </div>}
+                <h2>{config.label}</h2>
+                {!showPreview && <input
+                    type="file"
+                    accept="image/*"
+                    label="Add Image"
+                    onChange={handleAddImage}
+                />}
 
-            <h2>{config.label}</h2>
+                <div style={{ margin: "20px" }}>
+                    <button onClick={() => {
+                        if (!showPreview) {
+                            const canvas = canvasref.current;
+                            if (canvas) {
+                                canvas.discardActiveObject();
+                                canvas.renderAll();
+                            }
+                            setImageData(canvasref.current.toDataURL("image/png"))
+                        }
+                        setShowPreview(!showPreview)
+                    }}>preview</button>
+                </div>
 
-            <button onClick={() => {
-                if (!showPreview) {
-                    const canvas = canvasref.current;
-                    if (canvas) {
-                        canvas.discardActiveObject();
-                        canvas.renderAll();
+                <button onClick={() => {
+                    if (canvasref.current) {
+                        const canvas = canvasref.current;
+                        const activeObject = canvas.getActiveObject();
+                        if (activeObject) {
+                            canvas.remove(activeObject);
+                            canvas.renderAll();
+                        }
                     }
-                    setImageData(canvasref.current.toDataURL("image/png"))
-                }
-                setShowPreview(!showPreview)
-            }}>preview</button>
+                }}>Remove</button>
+                <div style={{ margin: "20px", display: "flex" }}>
+                    {showPreviousButton && <button onClick={() => {
+                        saveCanvas()
+                        setCanvasIndex(index - 1)
+                    }
+                    }>Previous</button>}
+                    {showNextButton && <button onClick={() => {
+                        saveCanvas()
+                        setCanvasIndex(index + 1)
+                    }}>Next</button>}
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Designer.js
+++ src/components/Designer.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 
 function Designer({ configFilePath }) {
     const [config, setConfig] = useState();
+    const [canvasIndex, setCanvasIndex] = useState(0);
+    const [canvasReferences, setCanvasReferences] = useState({});
+
     useEffect(() => {
         import(`../config/${configFilePath}`).then(data => {
             setConfig(data);
@@ -11,9 +14,24 @@ function Designer({ configFilePath }) {
     }, [])
     if (!config) return <div></div>
     return (
-        <div style={{ display: "flex" }}>
-            {config.components.map(data => {
-                return <Canvas id={data.id} key={data.id} config={data} />
+        <div>
+            {config.components.map((data, index) => {
+                if (canvasIndex !== index) return null;
+                return <Canvas id={data.id} key={data.id} config={data} searchForRef={() => {
+                    console.log("searched ref",canvasReferences[data.id]);
+                    return canvasReferences[data.id];
+                }} setCanvasRef={
+                    (ref) => {
+                        console.log("setting canvas ref", ref.current);
+                        setCanvasReferences((prev) => {
+                            return { ...prev, [data.id]: ref.current };
+                        });
+                    }
+                } showPreviousButton = {
+                    index > 0 ? true : false
+                } showNextButton = {
+                    index < config.components.length - 1 ? true : false
+                } setCanvasIndex={setCanvasIndex} index = {index} />
             })}
         </div>
     )
